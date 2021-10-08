37d17c3cc303ef225d7b23ec6ad0ab31efe3db1f
haoziqaq <357229046@qq.com>
2021-10-08 14:12:33 +0000

    Deploying to gh-page from @ haoziqaq/varlet@f529c309d41f4ca28ff8d3c0d00e48889837222d 🚀

--- assets/index.9097d2f0.js
+++
@@ -1,1 +0,0 @@
-import{P as n}from"./Pagination.c6944237.js";n.install=function(a){a.component(n.name,n)};
---
+++ assets/index.a6e626dd.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.c8959da1.js";n.install=function(a){a.component(n.name,n)};
