5b4d03678ff55d69ef108ea1326714bd18f842f8
haoziqaq <357229046@qq.com>
2021-10-27 16:04:18 +0000

    Deploying to gh-page from @ haoziqaq/varlet@536d7b1daa30b6b9a8093173f64e394d3a3e69eb 🚀

---
+++ assets/AppType.44c77136.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./elevation.c2941a7c.js";import{q as p,o as s,c as t,B as a}from"./vendor.ea41cd30.js";const n=p({name:"AppType"}),r={class:"app-type"};function _(e,c,d,i,l,f){return s(),t("div",r,[a(e.$slots,"default")])}var u=o(n,[["render",_]]);export{u as A};
---
+++ assets/index.0f7ce924.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.2af712af.js";n.install=function(a){a.component(n.name,n)};
