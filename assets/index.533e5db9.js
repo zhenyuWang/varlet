86bdcb7b1931b77f8e49fb7e81c1bb16c216c5f3
BeADre <beadre_wang@163.com>
2021-11-07 18:32:22 +0000

    Deploying to gh-page from @ haoziqaq/varlet@140e2bafe01bd48e07620c07264e72cef60d45f7 🚀

--- assets/index.533e5db9.js
+++
@@ -1,1 +0,0 @@
-import{P as n}from"./Pagination.5ea543f2.js";n.install=function(a){a.component(n.name,n)};
---
+++ assets/index.d94774de.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.e03d94d9.js";n.install=function(a){a.component(n.name,n)};
