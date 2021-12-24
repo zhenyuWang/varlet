fb562805ecaf493b1a3d8be37127f6860e923b1b
qytayh <39668309+qytayh@users.noreply.github.com>
2021-12-24 13:16:39 +0000

    Deploying to gh-page from @ haoziqaq/varlet@7c586d2f7e751a12993d06e46d460ac25de24b22 🚀

--- assets/index.99678bd4.js
+++
@@ -1,1 +0,0 @@
-import{P as n}from"./Pagination.9b816705.js";n.install=function(a){a.component(n.name,n)};
---
+++ assets/index.e99f498b.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.4b06f26b.js";n.install=function(a){a.component(n.name,n)};
