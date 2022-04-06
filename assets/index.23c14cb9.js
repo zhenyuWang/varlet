7e74b95a23faf6bc86cf3cb3421af747f1c6d306
zhenyuWang <13641039885@163.com>
2022-04-06 12:27:16 +0000

    Deploying to gh-page from @ zhenyuWang/varlet@f5710383a4ceffa8d6a33a6e8d98e84bcce7573c 🚀

---
+++ assets/index.23c14cb9.js
@@ -0,0 +1,1 @@
+import{P as n}from"./Pagination.1782833e.js";n.install=function(o){o.component(n.name,n)};
--- assets/index.7577810b.js
+++
@@ -1,1 +0,0 @@
-import{P as n}from"./Pagination.223a49e7.js";n.install=function(o){o.component(n.name,n)};
